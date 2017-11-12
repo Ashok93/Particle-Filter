a781f9d9a55f42699a4ab75431acbd714131a885
Ashok M <ashok.kannan93@gmail.com>
2017-11-12 22:02:07 +0100

    Cosmetic changes in spacing!

--- particle_filter.cpp
+++ particle_filter.cpp
@@ -8,14 +8,12 @@ double ParticleFilter::measurement_probability(std::vector<double> measurement,
                                         double noise)
 {
     double prob = 1.0;
-
     for(int i=0; i<measurement.size(); i++)
     {
         prob *= ParticleFilter::gaussian(measurement[i], noise, actual_measurement[i]);
     }
 
     return prob;
-
 }
 
 std::vector<Robot> ParticleFilter::resampling_wheel(std::vector<double> measurement_probabilities,
@@ -28,5 +26,6 @@ double ParticleFilter::gaussian(double mu, double sigma, double x)
 {
     double numerator = (std::pow((mu - x), 2)) / (std::pow(sigma, 2) / 2);
     double denominator = std::sqrt(2 * 3.1415926535897 * std::pow(sigma, 2));
+
     return std::exp(- (numerator / denominator));
 }
