1d640cb5102cb86a0a2ff31757857bd35bf77a7a
Ashok M <ashok.kannan93@gmail.com>
2017-11-15 09:36:53 +0100

    Implemented resampling phase of particle filter.

--- main.cpp
+++ main.cpp
@@ -16,20 +16,21 @@ int main()
     int world_size = 100;
     std::vector<std::vector<double>> landmark_positions = { {20,20}, {80,80}, {20,80}, {80,20} };
     Robot robot(0, 0, 0, world_size, landmark_positions);
-    robot.set_pose(5,5, PI);
+    robot.set_pose(10,10, PI);
     robot.move(0.1, 5.0);
+    std::cout << "---------The robot is at pose---------" << std::endl;
     robot.print_curr_pose();
+    std::cout << "-----------------------------------------------" << std::endl;
+
     std::vector<double> Z = robot.sense_landmarks();
 
-
     int N = 1000;
-
     std::vector<Robot> robot_particles;
 
     for(int i=0; i<N; i++)
     {
         Robot r(dist(mt)*world_size, dist(mt)*world_size, dist(mt)*2*PI, world_size, landmark_positions);
-        r.set_noise(0.05, 0.05, 5.0);
+        r.set_noise(0.005, 0.005, 2.0);
         r.move(0.1, 5.0);
         robot_particles.push_back(r);
     }
@@ -44,7 +45,8 @@ int main()
         measurement_probabilities.push_back(mp);
     }
 
-    std::cout << "measurement probabilities" << std::endl;
+    // printing the measurement probs.
+    std::cout << "---------measurement probabilities---------" << std::endl;
     for(int i=0; i<measurement_probabilities.size(); i++)
     {
         std::cout << measurement_probabilities[i] << std::endl;
@@ -54,9 +56,19 @@ int main()
             robot_particles[i].print_curr_pose();
         }
     }
+    std::cout << "-----------------------------------------------" << std::endl;
 
-    robot.move(-PI/2, 5);
-    robot.print_curr_dis_to_landmarks();
-    robot.print_curr_pose();
+    ParticleFilter pf;
+    std::vector<Robot> filtered_robots = pf.resampling_wheel(measurement_probabilities, robot_particles);
+
+    // Printing the selected particles.
+    std::cout << "--------Filtered Particles---------" << std::endl;
+    for(int i=0; i<filtered_robots.size(); ++i)
+    {
+        filtered_robots[i].print_curr_pose();
+    }
+
+    std::cout << "-----------------------------------------------" << std::endl;
+
     return 0;
 }
--- particle_filter.cpp
+++ particle_filter.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<vector>
+#include<random>
+#include<algorithm>
+
 #include "particle_filter.h"
 #include"robot.h"
 
@@ -19,7 +22,30 @@ double ParticleFilter::measurement_probability(std::vector<double> measurement,
 std::vector<Robot> ParticleFilter::resampling_wheel(std::vector<double> measurement_probabilities,
                                                     std::vector<Robot> particles)
 {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(0, 1);
 
+    std::vector<Robot> filtered_particles;
+    double index = dist(mt) * particles.size();
+    double beta = 0.0;
+
+    double max_measurement_prob = *std::max_element(measurement_probabilities.begin(), measurement_probabilities.end());
+
+    for(int i=0; i<particles.size(); ++i)
+    {
+        beta += dist(mt) * 2 * max_measurement_prob;
+
+        while(beta > measurement_probabilities[index])
+        {
+            beta -= measurement_probabilities[index];
+            index = fmod((index + 1),particles.size());
+        }
+
+        filtered_particles.push_back(particles[index]);
+    }
+
+    return filtered_particles;
 }
 
 double ParticleFilter::gaussian(double mu, double sigma, double x)
